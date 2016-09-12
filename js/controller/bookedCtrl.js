758b96e9b7e703f47a00a896c949220e3a14cea9
Carlos Sueldo <thetrip3@live.com>
2016-09-12 17:38:27 -0500

    displayed locations infovia locations ctrl

--- js/controller/bookedCtrl.js
+++ js/controller/bookedCtrl.js
@@ -1,5 +1,7 @@
 angular.module('devmtnTravel')
   .controller('bookedCtrl', function($scope, mainSrv){
+    // write a function that checks the url params and then loops over the data object in the service and then returns the object that matches the id being passed in the url params. Do this in your controller
+
 
 
 
--- js/controller/locationsCtrl.js
+++ js/controller/locationsCtrl.js
@@ -1,6 +1,7 @@
 angular.module('devmtnTravel')
   .controller('locationsCtrl', function($scope, mainSrv){
 
-
+    $scope.locations = mainSrv.travelInfo;
+    console.log($scope.locations);//use to check to make sure you are getting the propeties.
 
   });
